f217f34916b6def538b6f558e0d270116169c7f8
vikassharma1999 <vikas.aipro@gmail.com>
2020-06-02 21:53:26 +0530

    models added

---
+++ models/Contact.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const ContactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    contactno: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = Contact = mongoose.model('Contact', ContactSchema);

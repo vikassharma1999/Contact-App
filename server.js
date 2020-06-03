3a9062dee068c24de2eca7144a96ec241e730105
vikassharma1999 <vikas.aipro@gmail.com>
2020-06-03 23:26:05 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
@@ -19,6 +20,15 @@ app.use(cors());
 
 //contact router
 app.use('/api/contact', require('./routes/contact'));
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //PORT
 const PORT = process.env.PORT || 8000;
